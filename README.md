eb21890d670809ff8f5f92d0914ccf79b1a2cbde
GuiHSM <Guihenrisampaio@gmail.com>
2022-12-12 21:03:00 -0300

    Feito

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 <p> Durante o curso <a href="https://codigodofuturo.corporate.gama.academy/">Código do futuro</a> fiz diversos exercícios, este era um exercício sobre recursividade, segue o enunciado:</p>
 <h4>Enunciado</h4>
+<p>Faça um programa que leia itens de importação
+para isso você terá que criar um programa para ler arquivos json
+transformar estes arquivos em objetos
+armezenar os cliente na lista em memória
+mostrar o resultado na tela como um relatório
+
+
+terá uma pasta imports com os arquivos
+imports
+    cliente1.json
+    cliente2.json
+    cliente3.json
+    cliente4.json
+    cliente5.json   
+
+    Conteúdo do arquivo:
+        {"nome": "Danilo", "telefone": "123432123"}</p>
--- Recursividade/Recursividade/Program.cs
+++ Recursividade/Recursividade/Program.cs
@@ -1,2 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using Recursividade.Models;
+
+namespace menu;
+class Menu
+{
+
+    public const string CaminhoArquivo= "D:\\Tudo\\GitHub\\codigo-do-futuro-recursividade\\Recursividade\\Recursividade\\Clientes\\cliente";
+
+    public static void Main(string[] args)
+    {
+        Menu.criarMenu();
+    }
+
+    private static void criarMenu()
+    {
+        Console.WriteLine("Digite o número da operação escolhida:");
+        Console.WriteLine("1-Cadastro de Clientes");
+        Console.WriteLine("2-Listagem de Clientes");
+        Console.WriteLine("0-Sair");
+        int op = Int32.Parse(Console.ReadLine());
+        switch (op)
+        {
+            case 1:
+                Menu.cadastrar();
+                break;
+            case 2:
+                Console.Clear();
+                Menu.lerClientes(1);
+                Console.WriteLine("Pressione Enter para voltar");
+                string nada = Console.ReadLine();
+                break;
+            default:
+                return;
+        }
+        Menu.criarMenu();
+    }
+    private static string cadastrarCampo(string nomeCampo)
+    {
+        Console.WriteLine("Por favor, insira o " + nomeCampo + " do Cliente");
+        string campo = Console.ReadLine();
+        if (campo == "")
+        {
+            Console.Clear();
+            Console.WriteLine("Insiar um " + nomeCampo + " válido");
+            campo=Menu.cadastrarCampo(nomeCampo);
+        }
+        return campo;
+    }
+        private static void cadastrar()
+    {
+        Console.Clear();
+        Cliente cliente = new Cliente();
+        cliente.Nome = Menu.cadastrarCampo("Nome");
+        Console.Clear();
+        cliente.Telefone = Menu.cadastrarCampo("Telefone");
+        var JSON = JsonSerializer.Serialize<Cliente>(cliente);
+        File.WriteAllText(CaminhoArquivo + Menu.recolherId(1), JSON);
+    }
+    private static void lerClientes(int idAtual)
+    {
+        string json = "";
+        try
+        {
+            json = File.ReadAllText(CaminhoArquivo + idAtual);
+        }catch(Exception ex)
+        {
+            return;
+        }
+        var cliente=JsonSerializer.Deserialize<Cliente>(json);
+        Console.WriteLine("Nome: " + cliente?.Nome + ",Telefone: " + cliente?.Telefone);
+        Menu.lerClientes(idAtual + 1);
+    }
+    private static int recolherId(int idAtual)
+    {
+        string json = "";
+        try
+        {
+            json = File.ReadAllText(CaminhoArquivo + idAtual);
+        }
+        catch (Exception ex)
+        {
+            return idAtual;
+        }
+        return Menu.recolherId(idAtual + 1);
+    }
+    private static void escreverClientes()
+    {
+        
+    }
+}
